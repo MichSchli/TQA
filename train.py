81aed7766a9c02e479991f13349f76f48f45d307
MichSchli <michael.sejr@gmail.com>
2019-02-06 15:22:52 +0000

    updates

--- train.py
+++ train.py
@@ -5,13 +5,15 @@ from Mindblocks.interface import BasicInterface
 interface = BasicInterface()
 
 parser = argparse.ArgumentParser(description='Train (and test) a model with a given block.')
+parser.add_argument('--data')
 parser.add_argument('--block')
 args = parser.parse_args()
 
 block_filepath = "blocks/test.xml" #args.block
 block_name = ".".join(block_filepath.split('/')[-1].split(".")[:-1])
 
-data_filepath = "data/triviaqa-wiki"
+data_filepath = args.data
+data_filepath = "data/toy"
 interface.load_file(block_filepath)
 interface.set_variable("data_folder", data_filepath)
 interface.initialize()
